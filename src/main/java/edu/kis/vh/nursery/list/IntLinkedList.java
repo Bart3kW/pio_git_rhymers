32d7c3b1149aa8ff722f4193715731c3c0d6fc9f
Bart3kW <bartekpol83@onet.pl>
2025-04-05 12:33:25 +0200

    14. Usuniecie Node.java.

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,48 +2,46 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
-    private static final int ERROR = -1;
+	private static final int ERROR = -1;
 
-    private Node last;
-    private int value;
+	private Node last;
+	private int value;
 
-    public void push(int newValue) {
-        if (getLast() == null)
-            setLast(new Node(newValue));
-        else {
-            getLast().setNext(new Node(newValue));
-            getLast().getNext().setPrev(getLast());
-            setLast(getLast().getNext());
-        }
-    }
+	public void push(int newValue) {
+		if (getLast() == null)
+			setLast(new Node(newValue));
+		else {
+			getLast().setNext(new Node(newValue));
+			getLast().getNext().setPrev(getLast());
+			setLast(getLast().getNext());
+		}
+	}
 
-    public boolean isEmpty() {
-        return getLast() == null;
-    }
+	public boolean isEmpty() {
+		return getLast() == null;
+	}
 
-    public boolean isFull() {
-        return false;
-    }
+	public boolean isFull() {
+		return false;
+	}
 
-    public int top() {
-        if (isEmpty())
-            return ERROR;
-        return getLast().getValue();
-    }
+	public int top() {
+		if (isEmpty())
+			return ERROR;
+		return getLast().getValue();
+	}
 
-    public int pop() {
-        if (isEmpty())
-            return ERROR;
-        int ret = getLast().getValue();
-        setLast(getLast().getPrev());
-        return ret;
-    }
+	public int pop() {
+		if (isEmpty())
+			return ERROR;
+		int ret = getLast().getValue();
+		setLast(getLast().getPrev());
+		return ret;
+	}
 
 	private int getValue() {
 		return value;
 	}
-
-
 
 	private Node getLast() {
 		return last;
@@ -53,4 +51,35 @@ public class IntLinkedList {
 		this.last = last;
 	}
 
+	private class Node {
+
+		private final int value;
+		private Node prev;
+		private Node next;
+
+		public Node(int i) {
+			value = i;
+		}
+
+		Node getPrev() {
+			return prev;
+		}
+
+		void setPrev(Node prev) {
+			this.prev = prev;
+		}
+
+		Node getNext() {
+			return next;
+		}
+
+		void setNext(Node next) {
+			this.next = next;
+		}
+
+		int getValue() {
+			return value;
+		}
+
+	}
 }
